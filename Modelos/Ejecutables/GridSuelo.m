4592a87672bc592ec9031b3d876d1d1c6a94a161
marco <marco@DESKTOP-B46QL6U.home>
2023-11-07 01:45:58 +0100

    Modelos con una oruga, y con cuatro orugas completos (no incluye suelo) Modelo de cuatro orugas con suelo creado. Falta revisar fallos y velocidad de simulación

---
+++ Modelos/Ejecutables/GridSuelo.m
@@ -0,0 +1,12 @@
+%% Floor parameters
+Floor.l = 50;  % m
+Floor.w = 50;  % m
+Floor.h = 0.1; % m
+
+% Grid parameters
+Grid.clr = [1 1 1]*1;
+Grid.numSqrs = 150;
+Grid.lineWidth = 0.02;
+Grid.box_h = (Floor.l-(Grid.lineWidth*(Grid.numSqrs+1)))/Grid.numSqrs;
+Grid.box_l = (Floor.l-(Grid.lineWidth*(1+1)))/1;
+Grid.extr_data = Extr_Data_Mesh(Floor.w,Floor.w,Grid.numSqrs,1,Grid.box_h,Grid.box_l);
